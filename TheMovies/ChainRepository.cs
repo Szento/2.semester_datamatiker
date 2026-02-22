da980bcc950899eeeca705f67b366efcc205afe7
MarcoHessel <marcohessel1011@gmail.com>
2026-02-22 21:19:42 +0100

    LOCAL DATABASE Aquired

---
+++ TheMovies/ChainRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace TheMovies
+{
+    public class ChainRepository
+    {
+        private readonly string _connectionString =
+            "Server=localhost\\SQLEXPRESS;Database=TheMovies;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public int AddChain(string chainName)
+        {
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string sql = @"
+            INSERT INTO Chain (ChainName)
+            VALUES (@name);
+            SELECT CAST(SCOPE_IDENTITY() AS int);";
+
+            using SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@name", chainName);
+
+            int newId = (int)command.ExecuteScalar();
+
+            return newId;
+        }
+    }
+}
+    
---
+++ TheMovies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TheMovies
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Skriv kædens navn: ");
+            string name = Console.ReadLine() ?? "";
+
+            ChainRepository repo = new ChainRepository();
+            int id = repo.AddChain(name);
+
+            Console.WriteLine($"Chain gemt med ID: {id}");
+            Console.ReadKey();
+        }
+    }
+}
